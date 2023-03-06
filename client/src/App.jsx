3572ba267f1cf807a45c2b9d5d2b713101e5e2e9
Francisco Sabino <fdasabino@gmail.com>
2023-03-06 11:52:17 +0100

    Added routes and fetch connection data into dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,51 +1,27 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Button from "./components/Button";
-import { CONNECTION_ACTION_TYPES } from "./reducers-actions/connectionActions";
-import { BUTTON_TYPE_CLASSES } from "./components/Button";
+import { Routes, Route } from "react-router-dom";
+import { ws } from "./utils/webSocket";
+
+// Pages
+import Dashboard from "./pages/Dashboard/Dashboard";
+import CreateConnection from "./pages/CreateConnection/CreateConnection";
+import CreateJob from "./pages/CreateJob/CreateJob";
 
 const App = () => {
-  const dispatch = useDispatch();
-  let { connection } = useSelector((state) => ({ ...state }));
-
-  // Connection string and socket
-  const ws = new WebSocket("ws://localhost:8000");
-
   useEffect(() => {
     handleConnection();
-  }, []);
+  });
 
   const handleConnection = () => {
     ws.onopen = (res) => console.log("Open Connection ====>", res);
-    ws.onmessage = (res) => {
-      dispatch({
-        type: CONNECTION_ACTION_TYPES.LOAD_ALL_CONNECTIONS,
-        payload: JSON.parse(res.data),
-      });
-    };
-  };
-
-  const handleSendData = () => {
-    ws.send(JSON.stringify({ type: "MineService", action: "LOAD_ALL_CONNECTIONS" }));
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen ">
-      <h2 className="text-red-500 text-7xl">Group 1 Project</h2>
-      <div className="my-3">
-        <Button buttonType={BUTTON_TYPE_CLASSES.btn_primary} onClick={handleSendData}>
-          Click me!
-        </Button>
-        <Button buttonType={BUTTON_TYPE_CLASSES.btn_secondary} onClick={handleSendData}>
-          Click me!
-        </Button>
-        <Button buttonType={BUTTON_TYPE_CLASSES.btn_cancel} onClick={handleSendData}>
-          Click me!
-        </Button>
-      </div>
-
-      {JSON.stringify(connection)}
-    </div>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/create-new-connection" element={<CreateConnection />} />
+      <Route path="/create-new-job" element={<CreateJob />} />
+    </Routes>
   );
 };
 
---
+++ client/src/pages/CreateConnection/CreateConnection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateConnection = () => {
+  return <div>CreateConnection</div>;
+};
+
+export default CreateConnection;
