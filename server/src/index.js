4ef869dd85ab586a602ee851a9d048acbd47a767
Francisco Sabino <fdasabino@gmail.com>
2023-03-06 12:07:17 +0100

    optimized index.js server

--- server/src/index.js
+++ server/src/index.js
@@ -10,19 +10,20 @@ const server = app.listen(8000, () => {
 const wss = new WebSocket.Server({ server });
 
 const loadWsResponse = (message) => {
-  if (message.action === "CREATE_CONNECTION") {
-    if (message.connection.type === "se.iquest.iqmine.connections.SMHIConnection")
-      return createConnectionResponse(message.connection);
-    return { action: "CREATE_CONNECTION_FAILED", message: "Invalid connection" };
+  switch (message.action) {
+    case "CREATE_CONNECTION":
+      if (message.connection.type === "se.iquest.iqmine.connections.SMHIConnection")
+        return createConnectionResponse(message.connection);
+      return { action: "CREATE_CONNECTION_FAILED", message: "Invalid connection" };
+    case "RUN_EXPLORER":
+      if (message.job.type === "se.iquest.iqmine.poller.SMHIPoller")
+        return createJobResponse(message.job);
+      return { action: "RUN_EXPLORER_FAILED", message: "Invalid job type" };
+    default:
+      const wsResponse = responses.find((respone) => respone.action.startsWith(message.action)); //Returns object if found, otherwise returns undefined.
+      if (wsResponse) return wsResponse; //if wsResponse is not undefined
+      return { action: "ERROR", message: "Invalid command" };
   }
-  if (message.action === "RUN_EXPLORER") {
-    if (message.job.type === "se.iquest.iqmine.poller.SMHIPoller")
-      return createJobResponse(message.job);
-    return { action: "RUN_EXPLORER_FAILED", message: "Invalid job type" };
-  }
-  const wsResponse = responses.find((respone) => respone.action.startsWith(message.action)); //Returns object if found, otherwise returns undefined.
-  if (wsResponse) return wsResponse; //if wsResponse is not undefined
-  return { action: "ERROR", message: "Invalid command" };
 };
 
 wss.on("connection", (ws) => {
