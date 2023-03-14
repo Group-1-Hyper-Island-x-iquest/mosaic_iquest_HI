ebb2528d3b9783c52d99b84de4d8f1e0feef46a6
Jonathan Zeray <jonathan.zeray@hotmail.com>
2023-03-14 11:06:07 +0100

    Added progress bar + header text to job page

--- client/src/pages/Jobs/Jobs.jsx
+++ client/src/pages/Jobs/Jobs.jsx
@@ -1,8 +1,70 @@
 import "./Jobs.css";
 import React from "react";
+import { Steps, ConfigProvider } from "antd";
+
+import "./Jobs.css";
 
 const CreateJob = () => {
-  return <>CreateJob</>;
+  return (
+    <>
+      <div className="progress-bar-container">
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: "#F28F09",
+            },
+          }}
+        >
+          <Steps
+            className="progress-bar"
+            progressDot
+            size={"large"}
+            current={0}
+            items={[
+              {
+                title: "Select",
+              },
+              {
+                title: "Review",
+              },
+              {
+                title: "Save",
+              },
+            ]}
+          />
+        </ConfigProvider>
+      </div>
+      <h1 className="jobs-page-title">Select data points</h1>
+      <p className="jobs-page-description">
+        Select the data points you want to add to this connection
+      </p>
+    </>
+  );
 };
 
 export default CreateJob;
+
+// const CreateJob = () => {
+//   const description = 'This is a description';
+
+//   return <>
+//   <div className="progress-bar-container">
+//      <Steps
+//      className="progress-bar"
+//       progressDot
+//       current={1}
+//       items={[
+//         {
+//           title: 'Select'
+//         },
+//         {
+//           title: 'Review'
+//         },
+//         {
+//           title: 'Save'
+//         },
+//       ]}
+//     />
+//     </div>
+//   </>;
+// };
